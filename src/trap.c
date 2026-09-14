1bdfea363f7759944f191406c95aa75b32a552c8
pos <pgousdal@users.noreply.github.com>
2026-09-14 03:44:40 +0200

    M1: add GEMDOS BIOS XBIOS trap core

---
+++ src/trap.c
@@ -0,0 +1,55 @@
+#include "amtari.h"
+
+static int32_t dispatch_gemdos(struct amtari_context *ctx, uint16_t function)
+{
+    (void)ctx;
+    (void)function;
+    return AMTARI_ENOSYS;
+}
+
+static int32_t dispatch_bios(struct amtari_context *ctx, uint16_t function)
+{
+    (void)ctx;
+    (void)function;
+    return AMTARI_ENOSYS;
+}
+
+static int32_t dispatch_xbios(struct amtari_context *ctx, uint16_t function)
+{
+    (void)ctx;
+    (void)function;
+    return AMTARI_ENOSYS;
+}
+
+enum amtari_trap_kind amtari_trap_decode(unsigned int trap_number)
+{
+    switch (trap_number) {
+    case 1:
+        return AMTARI_TRAP_GEMDOS;
+    case 13:
+        return AMTARI_TRAP_BIOS;
+    case 14:
+        return AMTARI_TRAP_XBIOS;
+    default:
+        return AMTARI_TRAP_UNKNOWN;
+    }
+}
+
+int32_t amtari_trap_dispatch(struct amtari_context *ctx, unsigned int trap_number, uint16_t function)
+{
+    if (ctx == 0 || !ctx->initialized) {
+        return AMTARI_EINVAL;
+    }
+
+    switch (amtari_trap_decode(trap_number)) {
+    case AMTARI_TRAP_GEMDOS:
+        return dispatch_gemdos(ctx, function);
+    case AMTARI_TRAP_BIOS:
+        return dispatch_bios(ctx, function);
+    case AMTARI_TRAP_XBIOS:
+        return dispatch_xbios(ctx, function);
+    case AMTARI_TRAP_UNKNOWN:
+    default:
+        return AMTARI_ENOSYS;
+    }
+}

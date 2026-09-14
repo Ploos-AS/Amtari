ffc689cf9cc53bcf6a759132933c2f2647ed9672
pos <pgousdal@users.noreply.github.com>
2026-09-14 11:54:31 +0200

    M2.14: bump sectioned PRG milestone version

--- include/amtari.h
+++ include/amtari.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define AMTARI_VERSION "0.2.13-m2"
+#define AMTARI_VERSION "0.2.14-m2"
 #define AMTARI_PATH_MAX 260
 #define AMTARI_BASEPAGE_SIZE 256u
 #define AMTARI_EINVAL (-1)

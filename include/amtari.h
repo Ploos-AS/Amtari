f397f88853efae71d60de6b040c7e140774a6ef4
pos <pgousdal@users.noreply.github.com>
2026-09-15 01:59:43 +0200

    M2.27: add XBIOS Settime clock API

--- include/amtari.h
+++ include/amtari.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define AMTARI_VERSION "0.2.26-m2"
+#define AMTARI_VERSION "0.2.27-m2"
 #define AMTARI_PATH_MAX 260
 #define AMTARI_BASEPAGE_SIZE 256u
 #define AMTARI_MEM_BLOCK_MAX 32u
@@ -34,6 +34,7 @@ typedef int (*amtari_console_getc_fn)(void *opaque);
 typedef int (*amtari_console_putc_fn)(void *opaque, unsigned char ch);
 typedef int (*amtari_console_status_fn)(void *opaque);
 typedef int (*amtari_clock_get_fn)(void *opaque, uint32_t *tos_datetime);
+typedef int (*amtari_clock_set_fn)(void *opaque, uint32_t tos_datetime);
 typedef int32_t (*amtari_fs_open_fn)(void *opaque, const char *path, uint16_t mode);
 typedef int32_t (*amtari_fs_create_fn)(void *opaque, const char *path, uint16_t attr);
 typedef int32_t (*amtari_fs_close_fn)(void *opaque, int16_t handle);
@@ -54,7 +55,7 @@ struct amtari_console_io {
     amtari_console_status_fn output_ready;
     void *opaque;
 };
-struct amtari_clock_io { amtari_clock_get_fn get; void *opaque; };
+struct amtari_clock_io { amtari_clock_get_fn get; amtari_clock_set_fn set; void *opaque; };
 struct amtari_fs_io {
     amtari_fs_open_fn open; amtari_fs_create_fn create; amtari_fs_close_fn close;
     amtari_fs_read_fn read; amtari_fs_write_fn write; amtari_fs_seek_fn seek;
@@ -84,6 +85,8 @@ const char *amtari_version(void);
 int amtari_init(struct amtari_context *ctx);
 int amtari_random_seed(struct amtari_context *ctx, uint32_t seed);
 int amtari_clock_bind(struct amtari_context *ctx, amtari_clock_get_fn get_fn, void *opaque);
+int amtari_clock_bind_rw(struct amtari_context *ctx, amtari_clock_get_fn get_fn,
+                         amtari_clock_set_fn set_fn, void *opaque);
 int amtari_guest_memory_bind(struct amtari_context *ctx, uint8_t *data, size_t size);
 int amtari_guest_range_valid(const struct amtari_context *ctx, uint32_t address, size_t length);
 int amtari_guest_read16(const struct amtari_context *ctx, uint32_t address, uint16_t *value);

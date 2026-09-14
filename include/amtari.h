5984ba49462af22bca63b1324fedf3d0be6f9fc2
pos <pgousdal@users.noreply.github.com>
2026-09-14 20:02:21 +0200

    M2.21: bump version

--- include/amtari.h
+++ include/amtari.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define AMTARI_VERSION "0.2.20-m2"
+#define AMTARI_VERSION "0.2.21-m2"
 #define AMTARI_PATH_MAX 260
 #define AMTARI_BASEPAGE_SIZE 256u
 #define AMTARI_MEM_BLOCK_MAX 32u

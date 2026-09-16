d184d45429520b3350e70557e60b70198ee3c9d7
pos <pgousdal@users.noreply.github.com>
2026-09-16 12:35:19 +0200

    M3.12: add AES application lifecycle state

--- include/amtari.h
+++ include/amtari.h
@@ -2,7 +2,7 @@
 #define AMTARI_H
 #include <stddef.h>
 #include <stdint.h>
-#define AMTARI_VERSION "0.3.11-m3"
+#define AMTARI_VERSION "0.3.12-m3"
 #define AMTARI_PATH_MAX 260
 #define AMTARI_BASEPAGE_SIZE 256u
 #define AMTARI_MEM_BLOCK_MAX 32u
@@ -34,7 +34,7 @@ typedef int32_t (*amtari_aes_dispatch_fn)(void*,uint16_t,const int16_t*,uint16_t
 struct amtari_cpu_state { uint32_t d[8],a[8],pc; uint16_t sr; }; struct amtari_guest_memory { uint8_t *data; size_t size; };
 struct amtari_console_io { amtari_console_getc_fn getc; amtari_console_putc_fn putc; amtari_console_status_fn input_ready,output_ready; void *opaque; }; struct amtari_clock_io { amtari_clock_get_fn get; amtari_clock_set_fn set; void *opaque; };
 struct amtari_vdi_io { amtari_vdi_dispatch_fn dispatch; amtari_vdi_line_fn line; void *opaque; void *line_opaque; uint16_t width,height,colors,next_handle,line_color,line_style; };
-struct amtari_aes_io { amtari_aes_dispatch_fn dispatch; void *opaque; uint16_t next_application_id; };
+struct amtari_aes_io { amtari_aes_dispatch_fn dispatch; void *opaque; uint16_t next_application_id,current_application_id; uint8_t application_active; };
 struct amtari_fs_io { amtari_fs_open_fn open; amtari_fs_create_fn create; amtari_fs_close_fn close; amtari_fs_read_fn read; amtari_fs_write_fn write; amtari_fs_seek_fn seek; amtari_fs_delete_fn unlink; amtari_fs_mkdir_fn mkdir; amtari_fs_rmdir_fn rmdir; void *opaque; }; struct amtari_process_io { amtari_program_fetch_fn fetch; void *opaque; }; struct amtari_prg_info { uint32_t text_size,data_size,bss_size,symbol_size,flags; uint16_t absolute; }; struct amtari_mem_block { uint32_t address,size,owner_basepage; uint8_t valid,in_use; };
 struct amtari_context { enum amtari_machine machine; enum amtari_mode mode; struct amtari_cpu_state cpu; struct amtari_guest_memory memory; struct amtari_console_io console; struct amtari_clock_io clock; struct amtari_vdi_io vdi; struct amtari_aes_io aes; struct amtari_fs_io fs; struct amtari_process_io process; uint32_t drive_mask; uint8_t current_drive; char cwd[26][AMTARI_PATH_MAX]; uint32_t next_load_address,current_basepage; struct amtari_mem_block mem_blocks[AMTARI_MEM_BLOCK_MAX]; uint32_t heap_top,random_seed; uint8_t process_depth; int initialized; };
 const char *amtari_version(void); int amtari_init(struct amtari_context*); int amtari_random_seed(struct amtari_context*,uint32_t); int amtari_clock_bind(struct amtari_context*,amtari_clock_get_fn,void*); int amtari_clock_bind_rw(struct amtari_context*,amtari_clock_get_fn,amtari_clock_set_fn,void*);
